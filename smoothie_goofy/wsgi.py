1595d136c7e91ed53f61abdde915bc54054732d3
Patrick Huesler <patrick.huesler@gmail.com>
2014-08-17 17:31:04 +0200

    wsgi loads up heroku settings in heroku env

--- smoothie_goofy/wsgi.py
+++ smoothie_goofy/wsgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-if not os.environ.has_key("DJANGO_SETTINGS_MODULE"):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smoothie_goofy.settings")
-
-
 from django.core.wsgi import get_wsgi_application
 if os.environ.has_key("HEROKU"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smoothie_goofy.settings_heroku")
+
     from dj_static import Cling
     application = Cling(get_wsgi_application())
 else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smoothie_goofy.settings")
     application = get_wsgi_application()
